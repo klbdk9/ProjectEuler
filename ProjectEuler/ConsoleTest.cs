9d161f54f46ae676dc09079b6c804649ff385f1a
Kane L Desktop <unwrittenday@hotmail.com>
2019-02-12 02:54:51 -0600

    Initial add of completed Project Euler files to github.

---
+++ ProjectEuler/ConsoleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class ConsoleTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solutions1to25.BestChoice());
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
